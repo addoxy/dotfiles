b5f0d46f4d0bd5040f1fcfa56aa42cf910b7a696
Aditya Kumar <92105084+addoxy@users.noreply.github.com>
2025-07-06 00:30:30 +0530

    Update aliases

--- .alias.zsh
+++ .alias.zsh
@@ -2,12 +2,14 @@ alias h="~"
 alias ..='cd ..'
 alias ...='cd ../../'
 alias pg="cd ~/Personal-Github"
+alias wg="cd ~/Work-Github"
 alias py='python3'
 alias python='python3'
 
 alias zshrc="nano ~/.zshrc"
-alias alias-zsh="nano ~/.alias.zsh"
+alias aliasz="nano ~/.alias.zsh"
 alias reload="source ~/.zshrc"
+alias setup-next="pnpm add -D prettier tailwindcss-debug-screens prettier-plugin-tailwindcss prettier-plugin-organize-imports tailwindcss-animate && pnpm dlx shadcn@latest init"
 
 alias gitx=_gitx
 
